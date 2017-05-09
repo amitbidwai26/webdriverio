622a9ce71212714f1cfcfbda6bda29cf4fa34936
Amit Bidwai <amitbidwai.26@gmail.com>
2017-05-09 14:27:53 +0530

    Webdirver demo login and show user info

---
+++ README.md
@@ -0,0 +1,23 @@
+# WebdriverIO POC for Testing Splunk
+
+## Installation
+
+You must have [Node.js](https://www.nodejs.org/) installed.
+
+```sh
+npm install
+```
+
+## Usage
+
+Install the Dependencies:
+
+```sh
+npm install
+```
+
+Run the test server:
+
+```sh
+npm start
+```
---
+++ test/example.js
@@ -0,0 +1,28 @@
+
+var webdriverio = require('webdriverio');
+var secret = require('./secret.js')
+var options = { desiredCapabilities: { browserName: 'chrome' } };
+var client = webdriverio.remote(options);
+client
+    .init()
+    .url('https://www.splunk.com/en_us/homepage.html')
+    .click('.glyphicon-user')
+    .timeoutsImplicitWait(5000)
+    .url('https://login.splunk.com/index.php?module=roles&func=showloginform&redirecturl=https://www.splunk.com/')
+    .setValue('#username', secret.username)
+    .setValue('#password', secret.password )
+    .click('#login-submit')
+    .getUrl().then(function(url){
+        console.log('After Login////////////////////' + url);    
+        })
+    .url('https://www.splunk.com/index.php?module=roles&func=account')
+    .click('.glyphicon-user')
+    .getUrl().then(function(url){
+        
+        console.log('3 rd URL ***********' + url);    
+        })
+    .url('https://www.splunk.com/index.php?module=roles&func=account')
+    .timeoutsImplicitWait(5000)
+    .url('https://www.splunk.com/en_us/search.html?query=splunk')
+    .timeoutsImplicitWait(500000)
+    .end();
